79070c9bbb6607b1262d7f52fdff5c685b970ecb
Marco Minetti <marco.minetti@novetica.org>
2015-05-06 09:26:31 +0200

    Linted plugins/alert.js file.

--- plugins/alert.js
+++ plugins/alert.js
@@ -1,11 +1,17 @@
-exports.defineTags = function (dictionary) {
+/**
+    @overview Adds support for @alert tag through doclet.alert[] property.
+    @module plugins/alert
+    @author Marco Minetti <marco.minetti@novetica.org>
+ */
+'use strict';
 
+/*eslint spaced-line-comment: 0 */
+
+exports.defineTags = function (dictionary) {
     dictionary.defineTag('alert', {
-      
         onTagged: function (doclet, tag) {
             doclet.alert = doclet.alert || [];
             doclet.alert.push(tag.text);
         }
     });
 };
-
